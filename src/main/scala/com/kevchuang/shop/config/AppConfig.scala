62788bc5099e6ffc600a178281a47a69daf4f5de
kevin.chuang <kchuang.pro@gmail.com>
2023-06-18 12:31:07 +0200

    Add default AppConfig

--- src/main/scala/com/kevchuang/shop/config/AppConfig.scala
+++ src/main/scala/com/kevchuang/shop/config/AppConfig.scala
@@ -1,7 +1,16 @@
 package com.kevchuang.shop.config
 
 import cats.effect.kernel.Async
-import ciris.ConfigValue
+import ciris.*
+import ciris.refined.*
+import com.comcast.ip4s.*
+import eu.timepit.refined.*
+import eu.timepit.refined.auto.*
+import eu.timepit.refined.cats.*
+import eu.timepit.refined.types.string.*
+import cats.syntax.*
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.*
 
 final case class AppConfig(
     httpServerConfig: HttpServerConfig,
@@ -13,4 +22,22 @@ object AppConfig:
     default[F]
       .load[F]
 
-  private def default[F[_]]: ConfigValue[F, AppConfig] = ???
+  private def default[F[_]]: ConfigValue[F, AppConfig] =
+    (
+      env("SC_POSTGRES_PASSWORD").as[NonEmptyString].secret
+    ).map { (postgrePassword) =>
+      AppConfig(
+        HttpServerConfig(
+          host = host"0.0.0.0",
+          port = port"8080"
+        ),
+        PostgreSQLConfig(
+          host = "localhost",
+          port = 5432,
+          user = "postgres",
+          password = postgrePassword,
+          database = "store",
+          max = 10
+        )
+      )
+    }
