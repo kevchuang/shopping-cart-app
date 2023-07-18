df2e6b27bcea271da6290f56a41012c7727e11d2
kevin.chuang <kchuang.pro@gmail.com>
2023-07-19 00:57:54 +0200

    Implement findAll function

--- src/main/scala/com/kevchuang/shop/services/Brands.scala
+++ src/main/scala/com/kevchuang/shop/services/Brands.scala
@@ -1,16 +1,29 @@
 package com.kevchuang.shop.services
 
-import cats.effect.kernel.Resource
+import cats.effect.kernel.{MonadCancelThrow, Resource}
 import com.kevchuang.shop.domain.brand.*
-import skunk.Session
+import com.kevchuang.shop.sql.codecs.*
+import skunk.*
+import skunk.implicits.*
 
 trait Brands[F[_]]:
   def findAll: F[List[Brand]]
   def create(name: BrandName): F[BrandId]
 
 object Brands:
-  def make[F[_]](postgres: Resource[F, Session[F]]): Brands[F] =
+  def make[F[_]: MonadCancelThrow](
+      postgres: Resource[F, Session[F]]
+  ): Brands[F] =
+    import BrandsSQL.*
     new Brands[F]:
-      def findAll: F[List[Brand]] = ???
+      def findAll: F[List[Brand]] = postgres.use(_.execute(selectBrands))
 
       def create(name: BrandName): F[BrandId] = ???
+
+private object BrandsSQL:
+  private val brandDecoder: Decoder[Brand] = (brandId ~ brandName).map {
+    case (brandId, brandName) => Brand(brandId, brandName)
+  }
+
+  val selectBrands: Query[Void, Brand] =
+    sql"SELECT * FROM brands".query(brandDecoder)
