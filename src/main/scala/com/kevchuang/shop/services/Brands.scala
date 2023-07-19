b9e751e21f3ab893c6811db87958f877cfcf6964
kevin.chuang <kchuang.pro@gmail.com>
2023-07-19 14:30:38 +0200

    Add create function in Brands

--- src/main/scala/com/kevchuang/shop/services/Brands.scala
+++ src/main/scala/com/kevchuang/shop/services/Brands.scala
@@ -1,6 +1,9 @@
 package com.kevchuang.shop.services
 
+import cats.effect.*
+import cats.syntax.all.*
 import cats.effect.kernel.{MonadCancelThrow, Resource}
+import cats.effect.std.UUIDGen
 import com.kevchuang.shop.domain.brand.*
 import com.kevchuang.shop.sql.codecs.*
 import skunk.*
@@ -11,19 +14,30 @@ trait Brands[F[_]]:
   def create(name: BrandName): F[BrandId]
 
 object Brands:
-  def make[F[_]: MonadCancelThrow](
+  def make[F[_]: UUIDGen: MonadCancelThrow](
       postgres: Resource[F, Session[F]]
   ): Brands[F] =
     import BrandsSQL.*
     new Brands[F]:
       def findAll: F[List[Brand]] = postgres.use(_.execute(selectBrands))
 
-      def create(name: BrandName): F[BrandId] = ???
+      def create(name: BrandName): F[BrandId] = postgres.use { session =>
+        for
+          preparedCommand <- session.prepare(insertBrand)
+          uuid            <- UUIDGen.randomUUID[F].map(BrandId(_))
+          brandId         <- preparedCommand
+                               .execute(Brand(uuid, name))
+                               .as(uuid)
+        yield brandId
+      }
 
 private object BrandsSQL:
-  private val brandDecoder: Decoder[Brand] = (brandId ~ brandName).map {
+  private val brandCodec: Codec[Brand] = (brandId ~ brandName).imap {
     case (brandId, brandName) => Brand(brandId, brandName)
-  }
+  }(brand => (brand.uuid, brand.name))
 
   val selectBrands: Query[Void, Brand] =
-    sql"SELECT * FROM brands".query(brandDecoder)
+    sql"SELECT * FROM brands".query(brandCodec)
+
+  val insertBrand: Command[Brand] =
+    sql"INSERT INTO brands VALUES ($brandCodec)".command
