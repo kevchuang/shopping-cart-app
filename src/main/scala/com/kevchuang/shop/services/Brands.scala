ecb5a0c0a301146d5ee7546bc3a48b4d63f48da2
kevin.chuang <kchuang.pro@gmail.com>
2023-07-20 23:12:11 +0200

    Add create function in Brands service

--- src/main/scala/com/kevchuang/shop/services/Brands.scala
+++ src/main/scala/com/kevchuang/shop/services/Brands.scala
@@ -1,9 +1,9 @@
 package com.kevchuang.shop.services
 
 import cats.effect.*
-import cats.syntax.all.*
 import cats.effect.kernel.{MonadCancelThrow, Resource}
 import cats.effect.std.UUIDGen
+import cats.syntax.all.*
 import com.kevchuang.shop.domain.brand.*
 import com.kevchuang.shop.sql.codecs.*
 import skunk.*
@@ -24,10 +24,8 @@ object Brands:
       def create(name: BrandName): F[BrandId] = postgres.use { session =>
         for
           preparedCommand <- session.prepare(insertBrand)
-          uuid            <- UUIDGen.randomUUID[F].map(BrandId(_))
-          brandId         <- preparedCommand
-                               .execute(Brand(uuid, name))
-                               .as(uuid)
+          brandId         <- UUIDGen.randomUUID[F].map(BrandId(_))
+          _               <- preparedCommand.execute(Brand(brandId, name))
         yield brandId
       }
 
