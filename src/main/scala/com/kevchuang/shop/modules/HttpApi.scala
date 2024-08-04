4a8f7784131f9b0af3fab8a13fd876663bf9f0eb
Kevin Chuang <kchuang.pro@gmail.com>
2024-08-04 16:43:07 +0200

    Add CategoriesRoutes to HttpApi class

--- src/main/scala/com/kevchuang/shop/modules/HttpApi.scala
+++ src/main/scala/com/kevchuang/shop/modules/HttpApi.scala
@@ -2,7 +2,12 @@ package com.kevchuang.shop.modules
 
 import cats.effect.kernel.Async
 import cats.syntax.all.*
-import com.kevchuang.shop.http.routes.{BrandsRoutes, HealthRoutes, ItemsRoutes}
+import com.kevchuang.shop.http.routes.{
+  BrandsRoutes,
+  CategoriesRoutes,
+  HealthRoutes,
+  ItemsRoutes
+}
 import org.http4s.server.Router
 import org.http4s.*
 
@@ -10,10 +15,15 @@ abstract sealed class HttpApi[F[_]: Async] private (
     services: Services[F]
 ):
   private val brands: BrandsRoutes[F] = BrandsRoutes[F](services.brands)
+  private val categories: CategoriesRoutes[F] =
+    CategoriesRoutes[F](services.categories)
   private val health: HealthRoutes[F] = HealthRoutes[F](services.healthCheck)
   private val items: ItemsRoutes[F]   = ItemsRoutes[F](services.items)
   private val routes: HttpRoutes[F] =
-    brands.routes <+> health.routes <+> items.routes
+    brands.routes <+>
+      categories.routes <+>
+      health.routes <+>
+      items.routes
 
   val httpApp: HttpApp[F] = Router[F]("/" -> routes).orNotFound
 end HttpApi
